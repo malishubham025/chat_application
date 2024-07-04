da5b487f7e2e64cc4df784a43dfd3f13a1e937d1
malishubham025 <malishubham025@gmail.com>
2024-07-05 00:37:41 +0530

    base app socket

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const express=require("express");
-const app=express();
-const bodyParser=require("body-parser");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
-const http=require("http");
+const http = require("http");
 
-const server=http.createServer(app);
-const io=new Server(server);
-io.on("connection",(socket)=>{
-    console.log("connected");
-    console.log(socket.id);
-})
-app.get("/",(req,res)=>{
-    res.send("hi");
-})
-app.listen(3000,function(){
+app.set("view engine", "ejs");
+
+// Middleware to serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Create HTTP server
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io connection
+io.on("connection", (socket) => {
+    // console.log("connected");
+    // console.log(socket.id);
+    socket.on("message",(message)=>{
+        io.emit("message",message);
+        console.log("message received",message);
+    })
+
+});
+
+// Route to render index.ejs
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Start the server
+server.listen(3000, function() {
     console.log("running ...");
-})
+});
