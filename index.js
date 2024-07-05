0b748d176330ca7855bfcdae32232d346924e27e
malishubham025 <malishubham025@gmail.com>
2024-07-06 01:23:45 +0530

    basic chatapp build

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const { Server } = require("socket.io");
-const http = require("http");
-
-app.set("view engine", "ejs");
-
-// Middleware to serve static files
-app.use(express.static(__dirname + '/public'));
-
-// Create HTTP server
-const server = http.createServer(app);
-const io = new Server(server);
-
-// Socket.io connection
-io.on("connection", (socket) => {
-    // console.log("connected");
-    // console.log(socket.id);
+const express=require("express");
+const app=express();
+const http=require("http");
+const { connect } = require("http2");
+const server=http.createServer(app);
+const {Server}=require("socket.io");
+const io=new Server(server);
+app.set("view engine","ejs");
+var id="";
+io.on("connection",(socket)=>{
+    // console.log("connected",socket.id);
+    // id=socket.id;
+    console.log("connected",socket.id);
+    socket.emit("socketid",socket.id);
     socket.on("message",(message)=>{
-        io.emit("message",message);
-        console.log("message received",message);
+        console.log(message);
+        io.emit("receivedMessage",message);
+        // io.to().emit("receivedMessage",message);
     })
-
-});
-
-// Route to render index.ejs
-app.get("/", (req, res) => {
+    socket.on("messageto",(message)=>{
+        // console.log(message);
+        socket.emit("receivedMessage",message.value);
+        socket.to(message.to).emit("receivedMessage",message.value);
+    })
+    socket.on("joinroom",(id)=>{
+        console.log("user joinded",id);
+        socket.join(id);
+    })
+    
+})
+app.get("/",(req,res)=>{
     res.render("index");
-});
+})
 
-// Start the server
-server.listen(3000, function() {
-    console.log("running ...");
-});
+server.listen(3000,()=>{
+    console.log("running ");
+})
